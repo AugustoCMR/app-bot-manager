b1bbb601580e4feb756a88fc167cdecf1147c063
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2025-09-06 11:31:05 -0300

    wip: implementando CardBot

--- src/components/CardBot.tsx
+++ src/components/CardBot.tsx
@@ -1,8 +1,11 @@
 export const CardBot = () => {
   return (
-    <div className="border border-zinc-50 h-[200px] bg-zinc-800/50 rounded-lg">
-      <div className="p-2 text-zinc-50 font-bold">
+    <div className="border border-zinc-50/50 h-[200px] bg-zinc-800/50 rounded-lg flex flex-col cursor-pointer hover:bg-zinc-700/50 transition hover:scale-102">
+      <div className="p-3 text-zinc-50 font-bold">
         Titulo
+      </div>
+      <div className="p-3 text-zinc-50 font-bold mt-auto">
+        Status
       </div>
     </div>
   )
